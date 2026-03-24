4f763c8c200d82761ea62f024d8eacb26d6f9c75
Luis Cisnado <luiscisnadoia@gmail.com>
2026-03-24 17:03:27 -0600

    agrega script start

--- backend/Server.js
+++ backend/Server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const path = require("path");
 const pool = require("./db");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Servidor corriendo en puerto " + PORT);
+});
 
 const app = express();
 
